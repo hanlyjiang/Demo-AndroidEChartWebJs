c6b0a155c87e96f5a950d6dcff094b7e92667c95
hanlyjiang <hanlyjiang@outlook.com>
2016-08-03 17:04:58 +0800

    创建最简洁版本

--- AppTemplate/src/main/java/com/github/hanlyjiang/androidtemplate/MainActivity.java
+++ AppTemplate/src/main/java/com/github/hanlyjiang/androidtemplate/MainActivity.java
@@ -1,65 +1,17 @@
 package com.github.hanlyjiang.androidtemplate;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.util.ArrayMap;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-
-
-    private List<Map<String, Object>> mActivities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setStartItems();
-
-        SimpleAdapter adapter = new SimpleAdapter(this, mActivities, android.R.layout.simple_list_item_2,
-                new String[]{ "title", "desc"},
-                new int[]{android.R.id.text1, android.R.id.text2,});
-        ListView mItemList = (ListView) findViewById(R.id.listView);
-        mItemList.setAdapter(adapter);
-        mItemList.setOnItemClickListener(this);
 
     }
 
-
-    private void setStartItems() {
-        mActivities = new ArrayList<>();
-
-        mActivities.add(makeActItem(GoogleActivity.class,"测试Activitiy1","打开google"));
-
-        mActivities.add(makeActItem(BaiduActivity.class,"测试Activitiy2","打开百度"));
-
-    }
-
-    private Map<String, Object> makeActItem(Class activity, String title, String decs) {
-        Map<String, Object> mapItem;
-        mapItem = new ArrayMap<>();
-        mapItem.put("class", activity);
-        mapItem.put("title", title);
-        mapItem.put("desc", decs);
-        return mapItem;
-    }
-
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent();
-        @SuppressWarnings("unchecked")
-        Map<String, Object> item = (Map<String, Object>) parent.getAdapter().getItem(position);
-        intent.setClass(this, (Class<?>) item.get("class"));
-        startActivity(intent);
-    }
 }
